d78a94395468548db70eb3a632fd37690159099f
Fredrik Yumo <fyumo13@gmail.com>
2017-03-10 02:20:35 -0800

    added User Login and Blogs/Posts/Messages assignments

---
+++ Rails/blogs_posts_messages/app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ActiveRecord::Base
+  belongs_to :blog
+  has_many :messages
+  validates :title, presence: true, length: { minimum: 7 }
+  validates :content, presence: true
+
+  before_destroy :destroy_messages
+
+  private
+    # destroys all messages related to specific post
+    def destroy_messages
+      Message.where(post:self.id).destroy_all
+    end
+end
