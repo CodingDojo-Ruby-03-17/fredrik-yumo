4680c34d3263d1fb3667ce84639f460a34e2e27b
Fredrik Yumo <fyumo13@gmail.com>
2017-03-14 18:58:36 -0700

    updates Blogs/Posts/Messages

---
+++ Rails/blogs_posts_messages/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  has_many :messages
+  has_many :posts
+  has_many :owners
+  has_many :blogs, through: :owners
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+end
