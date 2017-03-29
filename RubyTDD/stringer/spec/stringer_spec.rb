cf711bad8e697abdb64aa4aa8187351a4819ddf5
Fredrik Yumo <fyumo13@gmail.com>
2017-03-28 18:23:40 -0700

    updated Stringer assignment

--- RubyTDD/stringer/spec/stringer_spec.rb
+++ RubyTDD/stringer/spec/stringer_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe Stringer do
     expect(Stringer.minify("Hello!", 10)).to eq("Hello!")
   end
 
-  it 'replaces a given word in a string with a provided replacement word' do
+  it "replaces a given word in a string with a provided replacement word" do
     expect(Stringer.replacify("I can\'t do this", "can\'t", "can")).to eq("I can do this")
   end
 
-  it 'places each word in a string into an array' do
+  it "places each word in a string into an array" do
     expect(Stringer.tokenize("I love to code")).to eq(["I", "love", "to", "code"])
   end
 
-  it 'removes each instance of a given word' do
+  it "removes each instance of a given word" do
     expect(Stringer.removify("I\'m not a developer", "not")).to eq("I\'m a developer")
   end
 end
